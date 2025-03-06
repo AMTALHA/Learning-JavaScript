21abff46b5182087064841c2b619b3c14313fd85
Talha <amtalhabd@gmail.com>
2025-03-06 21:26:17 +0600

    Some Changes

---
+++ 14.String Methods/script.js
@@ -0,0 +1,30 @@
+// let username = "Talha";
+
+// console.log(username.charAt(0));
+
+// let username = 'BroCode';
+// console.log(username.indexOf("o"));
+
+// let username = "BroCode";
+// console.log(username.lastIndexOf("o"));
+
+// let username = "BroCode";
+// console.log(username.length);
+
+// let username = "BroCode";
+// username = username.trim();
+// console.log(username);
+
+// let username = "BroCode";
+
+// username = username.toUpperCase();
+// console.log(username);
+
+// username = username.toLowerCase();
+// console.log(username);
+
+
+let username = "BroCode";
+
+username = username.repeat(5);
+console.log(username);
