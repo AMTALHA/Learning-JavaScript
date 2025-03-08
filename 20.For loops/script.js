2fbc6fd6e082df27b1e771e576a3d77448e7aa7a
Talha <amtalhabd@gmail.com>
2025-03-08 14:04:48 +0600

    Some Changes

---
+++ 20.For loops/script.js
@@ -0,0 +1,16 @@
+// for loop = repete some code for limited amount of time
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// console.log("Happy New Year!");
+
+for (i = 1; i <= 20; i++) {
+
+  if (i == 13) {
+    // continue; it will skip the number 13 and continue to next one
+    // break; it will end here
+  }
+  console.log(i);
+}
