cb72d150e02191eb54b3938202fb51df4770d0f9
Talha <amtalhabd@gmail.com>
2025-03-09 10:49:26 +0600

    Some Changes

---
+++ 22.Functions/script.js
@@ -0,0 +1,53 @@
+// function happyBirthday(username, age) {
+//   console.log('Happy Birthday to You!');
+//   console.log('Happy Birthday to You!');
+//   console.log(`Happy Birthday dear ${username}!`);
+//   console.log(`You are ${age} years old!`)
+// }
+
+// happyBirthday("Talha", 12);
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+
+// console.log(add(2, 3));
+
+// function substract(x, y) {
+//   return x - y;
+// }
+
+// console.log(substract(2, 3));
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// console.log(multiply(2, 3));
+
+// function divide(x, y) {
+//   return x / y;
+// }
+
+// console.log(divide(2, 3));
+
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isEven(20));
+
+function isValidEmail(email) {
+  if (email.includes("@")) {
+    return true;
+  } else {
+    return false;
+  }
+  }
+
+console.log(isValidEmail("talha@gmail.com"));
---
+++ 22.Functions/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+function isValidEmail(email) {
+  if (email.inclu
