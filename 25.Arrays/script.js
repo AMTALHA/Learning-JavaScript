adccc6156e9229ed88b51290844f01f6e06baf7e
Talha <amtalhabd@gmail.com>
2025-03-09 14:34:07 +0600

    Some Changes

---
+++ 25.Arrays/script.js
@@ -0,0 +1,29 @@
+// array = a variable like structure that
+//         can hold more than 1 value
+
+
+let fruits = ["apple", "orenge", "watar melon"];
+
+// fruits[0] = "coconut";
+
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("coconut");
+// fruits.shift();
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+
+// let numOfFruits = fruits.length;
+// console.log(numOfFruits);
+
+// let index = fruits.indexOf("orenge");
+
+// console.log(index);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
